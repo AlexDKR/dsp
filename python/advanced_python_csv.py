68a43d8c2a2f67c6661e29eb53d5009b85e39c85
ADK <eightykr@gmail.com>
2016-12-29 16:30:11 -0800

    finished 5b-csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,8 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+import re
+
+# convert to dataframe
+faculty_df = pd.read_csv('faculty.csv')
+
+# Q5 take e-mail list and write to a csv
+email_list = faculty_df[' email'].to_csv('emails.csv', index = False)
